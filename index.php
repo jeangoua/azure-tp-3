a7d9f9e24a8fe81f0606775ed16b262bf3858923
Jean <jgoua@iutbayonne.univ-pau.fr>
2021-12-14 16:37:33 +0100

    pdo

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
 <?php 
 echo "BIENVENUE\n";
 echo "\nC'est une page php";
-echo "\n3";
+echo "\n1";
 
-            $servername = 'bdd-mysql.mysql.database.azure.com';
-            $username = 'jgoua@bdd-mysql';
-            $password = 'P@ssword24330';
-            
-            //On établit la connexion
-            $conn = new mysqli($servername, $username, $password);
-            
-            //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
-            } else {
-                echo 'Connexion réussie';
-            }
-            
-            $req = $conn->mysql_query("SELECT * FROM Ebike");
-            while($data = mysql_fetch_assoc($req)){
-                // on affiche les informations de l'enregistrement en cours
-                echo '<b>'.$data['id'].' '.$data['name'].'</b> ('.$data['price'].')';
-                }
-            var_dump($result);
-// var_dump($recipes);
+$user = 'jgoua@bdd-mysql';
+$password = 'P@assword24330';
 
+$db = new PDO(
+    'mysql:host=bdd-mysql.mysql.database.azure.com;dbname=bikes;charset=utf8',
+    $user,
+    $password
+);
 
-    
+foreach($db->exec('SELECT * FROM Ebike') as $row) {
+    echo $row['name'];
+}
 ?>
