efdb4ed54949bb2fd2e0b25eec7d19124618dda7
Jean <jgoua@iutbayonne.univ-pau.fr>
2021-12-14 15:16:37 +0100

    connexion db

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
+
+$db = new PDO(
+    'mysql:host=bdd-mysql.mysql.database.azure.com;dbname=Bikes;charset=utf8',
+    'jgoua@bdd-mysql',
+    'P@ssword24330'
+);
+
+$recipesStatement = $db->prepare('SELECT * FROM recipes');
+$recipesStatement->execute();
+$recipes = $recipesStatement->fetchAll();
+
+
     echo "BIENVENUE";
     echo "C'est une page php";
 ?>
