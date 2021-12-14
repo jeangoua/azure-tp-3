b00da18d52ee3ca1447fbe572971813d9a687d99
Jean <jgoua@iutbayonne.univ-pau.fr>
2021-12-14 17:16:17 +0100

    finished

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php 
-ini_set('error_reporting', 1);
-ini_set('display_errors', 1);
+try
+   {
+      // On se connecte à MySQL
+      $mysqlClient = new PDO('mysql:host=bdd-mysql.mysql.database.azure.com;dbname=bikes;charset=utf8', 'jgoua', 'P@assword24330');
+   }
+   catch(Exception $e)
+   {
+      // En cas d'erreur, on affiche un message et on arrête tout
+      die('Erreur : '.$e->getMessage());
+   }
 
-echo "BIENVENUE\n";
-echo "\nC'est une page php";
-echo "\n1";
+   echo 'Connexion reussie';
+   // Si tout va bien, on peut continuer
 
-$user = 'jgoua@bdd-mysql';
-$password = 'P@assword24330';
-try {
-    $db = new PDO(
-        'mysql:host=bdd-mysql.mysql.database.azure.com;dbname=bikes;charset=utf8',
-        $user,
-        $password
-    );
-    
-    foreach($db->exec('SELECT * FROM Ebike') as $row) {
-        echo $row['name'];
-    }
-    
-} catch(PDOexeption $e) {
-    //echo $e->getmessage();
-}
+   // On récupère tout le contenu de la table utilisateur
+   $sqlQuery = 'SELECT * FROM bike';
+   $userStatement = $mysqlClient->prepare($sqlQuery);
+   $userStatement->execute();
+   $user = $userStatement->fetchAll();
 
+   // On affiche chaque recette une à une
+   foreach ($user as $recipe) {
+      echo $recipe['name'];
+   }
 ?>
