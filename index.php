7a9ba8586a9f4f6b1408e510c22a011d44da623f
Jean <jgoua@iutbayonne.univ-pau.fr>
2021-12-14 16:06:03 +0100

    test

--- index.php
+++ index.php
@@ -9,14 +9,10 @@ $db = new PDO(
 $recipesStatement = $db->prepare('SELECT * FROM recipes');
 $recipesStatement->execute();
 $recipes = $recipesStatement->fetchAll();
-
+var_dump($recipes);
 foreach ($recipes as $recipe) {
     echo $recipe;
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> e08cf5e639a5704814e23202a36caffbdf9c35a0
 
     echo "BIENVENUE\n";
     echo "C'est une page php";
